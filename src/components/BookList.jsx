227c19a08becbe00d5d1f5b992fa1044204475b8
henry-thiongi <boblilhot@gmail.com>
2022-10-07 02:43:07 +0300

    initialize

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import '../App';
+import { API_URL } from '../API';
+import axios from 'axios';
 
 const BookList = () => {
-  return (
-    <div className='book-list'>BookList</div>
-  );
+    
+    const [books,setBooks] = useState([]);
+
+
+    useEffect(()=> {
+      axios
+      .get(API_URL)
+      .then(res=> {
+        console.log(res.data);
+        setBooks(res.data);
+      })
+      .catch(err=>console.log(err));
+ },[]);
+
+return <div className='book-list'>
+    {books.map((book)=> (
+        <div key={book.id}>
+            <div><h2>{book.title}</h2></div>
+            <div><img src={book.image_url} alt="#"/></div>
+            <div><button>Add to Favorite</button></div>
+    </div>
+    ))}
+    </div>;
+  
 };
 
 export default BookList;
