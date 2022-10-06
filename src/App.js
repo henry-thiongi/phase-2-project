02949278c2d28fced12a48dbd7159794088698ab
henry-thiongi <boblilhot@gmail.com>
2022-10-06 05:53:19 +0300

    initialize

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import BookList from './components/BookList';
 import BookDetails from './components/BookDetails';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Favorites from './components/Favorite';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
       <Route path="/" element={<BookList />} />
       <Route path="/book/:id" element={<BookDetails />} />
-      <Route path="/" element={<BookList />} />
+      <Route path="/favorites" element={<Favorites />} />
       </Routes>
+      <Footer />
       
     </div>
   );
